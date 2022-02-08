3367a7ad4912d0e664875cf9b2c8b8cc034421c5
ACristoff <acristoffanini@gmail.com>
2022-02-07 22:05:41 -0800

    implementation of authorization states

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import { GoogleLogin } from 'react-google-login';
 
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Icon from './Icon';
 import useStyles from './styles'
-import { deprecationHandler } from 'moment';
+
+import Input from './Input';
 
 const Auth = () => {
   const classes = useStyles();
-  
-  const isSignup = false;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword )
+
   const handleSubmit = () => {
 
   }
@@ -16,6 +23,19 @@ const Auth = () => {
   const handleChange = () => {
     
   }
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false)
+  }
+
+  const googleSuccess = (res) => {
+    console.log(res)
+  };
+
+  const googleFailure = () => {
+    console.log("Google Sign In was unsuccesful. Try again later.")
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -29,11 +49,35 @@ const Auth = () => {
             {
               isSignup && (
                 <>
-                  <TextField name='firstName' label="First Name" handleChange={handleChange} autoFocus xs={6} />
-                  <TextField name='lastName' label="Last Name" handleChange={handleChange} autoFocus xs={6} />
+                  <Input name='firstName' label="First Name" handleChange={handleChange} autoFocus half />
+                  <Input name='lastName' label="Last Name" handleChange={handleChange} half />
                 </>
               )
             }
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+            {isSignup && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password" />}
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            {isSignup ? 'Sign Up' : 'Sign In'}
+          </Button>
+          <GoogleLogin 
+            clientId='380738796876-e9nsclmj0c4f83ebf9e4sopi987knm3j.apps.googleusercontent.com'
+            render={(renderProps) => (
+              <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+                Sign in with Google
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy='single_host_origin'
+          />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up!"}
+              </Button>
+            </Grid>
           </Grid>
         </form>
       </Paper>
