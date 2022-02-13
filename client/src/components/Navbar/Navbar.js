c4c80de75785602fc743d673146423ada90fc955
ACristoff <acristoffanini@gmail.com>
2022-02-13 14:22:44 -0800

    token expiry functionality

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+import decode from 'jwt-decode'
 
 import useStyles  from './styles';
 import memories from '../../images/memories.png'
@@ -25,6 +26,13 @@ const Navbar = () => {
     const token = user?.token;
 
     //JWT...
+    if (token) {
+      const decodedToken = decode(token);
+
+      if(decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')))
   }, [location]);
