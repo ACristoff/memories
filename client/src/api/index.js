0a2fded2fa83f6fc1e603f7f61fa836f8cad8a24
ACristoff <acristoffanini@gmail.com>
2022-03-14 14:43:47 -0700

    addition of a readme

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3001'});
+const API = axios.create({ baseURL: 'https://memories-project-acristoff.herokuapp.com'});
 //for deployment
 //const API =  axios.create({ baseURL: 'https://memories-project-acristoff.herokuapp.com'});
 //for development
